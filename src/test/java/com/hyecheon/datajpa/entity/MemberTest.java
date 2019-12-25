937fc179fd721622a3ea264f00ec5fed4ff17a16
hyecheon <rainbow880616@gmail.com>
2019-12-25 23:07:05 +0900

    Auditing

--- src/test/java/com/hyecheon/datajpa/entity/MemberTest.java
+++ src/test/java/com/hyecheon/datajpa/entity/MemberTest.java
@@ -1,6 +1,8 @@
 package com.hyecheon.datajpa.entity;
 
+import com.hyecheon.datajpa.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,8 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void testEntity() {
@@ -45,6 +49,26 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.team = " + member.getTeam());
         }
+
+    }
+
+    @Test
+    void JpaEventBaseEntity() throws InterruptedException {
+        final Member member = new Member("member1");
+        memberRepository.save(member);
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        final Member findMember = memberRepository.findById(member.getId()).get();
+
+        System.out.println("findMember.getCreatedDate = " + findMember.getCreateDate());
+        System.out.println("findMember.getCreatedBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.getUpdateDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getLastModifiedBy = " + findMember.getLastModifiedBy());
 
     }
 }
