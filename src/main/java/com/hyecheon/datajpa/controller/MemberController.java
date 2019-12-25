6c4ddc85f549202ec303bf02cca1500072b2ecc0
hyecheon <rainbow880616@gmail.com>
2019-12-25 23:29:39 +0900

    페이징과 정렬

--- src/main/java/com/hyecheon/datajpa/controller/MemberController.java
+++ src/main/java/com/hyecheon/datajpa/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.hyecheon.datajpa.controller;
 
+import com.hyecheon.datajpa.dto.MemberDto;
 import com.hyecheon.datajpa.entity.Member;
 import com.hyecheon.datajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +30,17 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
         memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+
+        }
     }
 }
