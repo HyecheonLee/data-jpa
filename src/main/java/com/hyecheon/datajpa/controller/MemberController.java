374c2080d32f36ea0e642fbdb0406588d121274f
hyecheon <rainbow880616@gmail.com>
2019-12-25 23:12:32 +0900

    도메인 클래스 컨버터

---
+++ src/main/java/com/hyecheon/datajpa/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.hyecheon.datajpa.controller;
+
+import com.hyecheon.datajpa.entity.Member;
+import com.hyecheon.datajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        final Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
